a389e8a76ad35ea82ef9c0b4a9bab9ffc892dd0e
Lucas Baizer <titaniumsapphire32@gmail.com>
2018-03-22 21:54:33 -0700

    Removed sample code

--- src/io/github/lucasbaizer/kjc/Main.java
+++ src/io/github/lucasbaizer/kjc/Main.java
@@ -1,9 +1,19 @@
 package io.github.lucasbaizer.kjc;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter the lobby code: ");
+		int lobby = Integer.parseInt(scanner.nextLine());
+
+		System.out.print("Enter your username: ");
+		String name = scanner.nextLine();
+
 		try {
-			KahootClient client = new KahootClient(3828407, "Jacob");
+			new KahootClient(lobby, name);
 		} catch (KahootException e) {
 			System.out.println("Error with client: " + e.getMessage());
 			if (e.getCause() != null) {
@@ -14,6 +24,8 @@ public class Main {
 		} catch (Exception e) {
 			e.printStackTrace(System.out);
 			System.exit(1);
+		} finally {
+			scanner.close();
 		}
 	}
 }
